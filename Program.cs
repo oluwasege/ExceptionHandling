9304037bde97eb5ccd9888d7182f20bfde514cec
Oluwasegun <akinpelu53@gmail.com>
2021-05-18 14:59:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+            {
+                StreamReader streamReader = new StreamReader(@"C:\smple.txt");
+               
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
